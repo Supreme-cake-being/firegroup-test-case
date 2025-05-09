87a565f8d392c0f7def3927bcad494827ce0484e
Karr4 but from another universe <XGon20987@gmail.com>
2025-05-09 21:02:41 +0300

    added masonry layout

--- frontend/src/components/BlogList.tsx
+++ frontend/src/components/BlogList.tsx
@@ -24,37 +24,53 @@ export const BlogList = ({ blogs }: IBlogList) => {
 
   return (
     <>
-      <Button color="primary" onPress={handleCreateButtonClick}>
+      <Button
+        color="primary"
+        onPress={handleCreateButtonClick}
+        className="mr-auto"
+      >
         Create Post
       </Button>
 
-      <div className="mt-2 flex justify-center gap-4 flex-wrap">
-        {blogs?.map(({ _id, title, text, image }: Record<string, any>) => (
-          <Card key={_id} className="py-4 w-full md:w-[320px] lg:w-[360px]">
-            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-              <h4 className="font-bold text-large">{title}</h4>
-              <p className="text-ellipsis overflow-hidden w-full whitespace-nowrap">
-                {text}
-              </p>
-            </CardHeader>
-            <CardBody className="overflow-visible py-2">
-              <Image
-                alt={title}
-                src={image ? image.url : FallBackImage.src}
-                className="w-full h-auto"
-                width={0}
-                height={0}
-              />
-            </CardBody>
-            <CardFooter className="flex justify-end gap-1">
-              <DeleteModal id={_id} title={title} />
-              <EditButton
-                text="Edit post"
-                onPress={() => push(`/blog/edit/${_id}`)}
-              />
-            </CardFooter>
-          </Card>
-        ))}
+      <div className="mt-4 columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
+        {blogs?.map(
+          ({ _id, title, text, image, createdAt }: Record<string, any>) => (
+            <Card key={_id} className="w-full md:w-[236px] lg:w-[300px]">
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">{title}</h4>
+                <p className="text-ellipsis overflow-hidden w-full whitespace-wrap">
+                  {text}
+                </p>
+              </CardHeader>
+              <CardBody className="overflow-visible py-2">
+                <Image
+                  alt={title}
+                  src={image ? image.url : FallBackImage.src}
+                  className="w-full h-auto"
+                  width={0}
+                  height={0}
+                />
+              </CardBody>
+              <CardFooter className="flex justify-between">
+                <p>
+                  {new Intl.DateTimeFormat("en-GB", {
+                    day: "2-digit",
+                    month: "short",
+                    year: "numeric",
+                  }).format(new Date(createdAt))}
+                </p>
+
+                <div className="flex gap-1">
+                  <DeleteModal id={_id} title={title} />
+                  <EditButton
+                    text="Edit post"
+                    onPress={() => push(`/blog/edit/${_id}`)}
+                  />
+                </div>
+              </CardFooter>
+            </Card>
+          )
+        )}
       </div>
     </>
   );
